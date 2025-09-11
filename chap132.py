8584c6326cd5f0f01b16474fa976dfa65bc70771
mingi0118 <p3552017@gmail.com>
2025-09-11 16:38:30 +0900

    Delete chap132.py

--- chap132.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-img1 = cv2.imread('Dog.jpg')
-img2 = cv2.imread('OpenCV_Logo_with_text.png')
-dst = cv2.addWeighted(img1,0.7,img2,0.3,0)
-cv2.imshow('dst',dst)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
